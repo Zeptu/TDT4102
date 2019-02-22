c6d55e978ca10ec80710d6dfb295ef4441747499
Zeptu <42481829+Zeptu@users.noreply.github.com>
2019-02-22 10:35:30 +0100

    Update Oppgave_3.cpp

--- Oppgave_3.cpp
+++ Oppgave_3.cpp
@@ -17,42 +17,42 @@ EKSEMPELKODE:
 /*
 FORKLARING: operator OPERATOR(eks; '<<' )
 (Parametere: 1) Venstre side av operatoren, velger hva som kan skrives inn
- Parameter: 2) Høyre side av operatoren, her spesifiserer du f.eks hva som kan skrives ut.
+ Parameter: 2) HÃ¸yre side av operatoren, her spesifiserer du f.eks hva som kan skrives ut.
 operator <<(ostream&, const CourseCatalog& c
 
 VS: ostream& os --> Du gir en ostream (eks. cout, ostream eller fil) samme funksjonaliteten som cout
-HS: const CourseCatalog& c --> Du kan nå skrive ut dette å samme måte som du kan skrive ut en int, string, etc.)
+HS: const CourseCatalog& c --> Du kan nÃ¥ skrive ut dette Ã¥ samme mÃ¥te som du kan skrive ut en int, string, etc.)
 
 ostream& --> Operatorene returnerer en ostream referanse
 */
 
-void CourseCatalog::addCourse(string courseCode, string courseName) {			//legger til både nøkler og verdier i map
-	//courseMap.insert({courseCode, courseName});		//insert							//bruker map_navn.insert - syntaksen (skjønte ikke helt den andre metoden)
+void CourseCatalog::addCourse(string courseCode, string courseName) {		//legger til bÃ¥de nÃ¸kler og verdier i map
+	//courseMap.insert({courseCode, courseName});		//insert	//bruker map_navn.insert - syntaksen (skjÃ¸nte ikke helt den andre metoden)
 	courseMap[courseCode] = courseName;			//[]	
-	/*for (const auto& i : courseMap) {									//to cout or not to cout, that! is the question (tror det er lurere å coute i main egt)
+	/*for (const auto& i : courseMap) {					//to cout or not to cout, that! is the question (tror det er lurere Ã¥ coute i main egt)
 	cout << i.first << ": " << i.second << endl;*/
 }
 //[] tilordner CourseCode en ny verdi, mens insert ikke endrer hvis det allerede eksisterer
 
 
-void CourseCatalog::removeCourse(string courseCode) {							//removeCourses fjerner emner fra map
+void CourseCatalog::removeCourse(string courseCode) {				//removeCourses fjerner emner fra map
 	courseMap.erase(courseCode);
 }
 
-string CourseCatalog::getCourse(string courseCode) {							//getCourses finner emnenavnet til kurs ved gitt emnekode
-	string getName = courseMap[courseCode];										//map_navn[nøkkel] - syntaks henter ut verdien som tilsvarer gitt nøkkel
+string CourseCatalog::getCourse(string courseCode) {				//getCourses finner emnenavnet til kurs ved gitt emnekode
+	string getName = courseMap[courseCode];					//map_navn[nÃ¸kkel] - syntaks henter ut verdien som tilsvarer gitt nÃ¸kkel
 	return getName;
 }
 
-void CourseCatalog::addCourses() {												//medlemsfunksjonen addCourses lar bruker lage en emnekatalog og lagrer i et Map med emnekode som nøkkel
+void CourseCatalog::addCourses() {						//medlemsfunksjonen addCourses lar bruker lage en emnekatalog og lagrer i et Map med emnekode som nÃ¸kkel
 	int numCourses{ 0 };
 	string courseCodee{ "" };
 	string courseNamee{ "" };
 
-	cout << "Hvor mange emner ønsker du å legge til?" << endl;					//spør bruker om antall emner
+	cout << "Hvor mange emner Ã¸nsker du Ã¥ legge til?" << endl;		//spÃ¸r bruker om antall emner
 	cin >> numCourses;
 
-	for (int i = 0; i < numCourses; ++i) {										//for-løkken som ber om emner, iterasjoner spesifisert av bruker
+	for (int i = 0; i < numCourses; ++i) {					//for-lÃ¸kken som ber om emner, iterasjoner spesifisert av bruker
 		cout << "Emnets emnekode: " << endl; cin >> courseCodee;
 		cout << "Emnenavn: " << endl; cin >> courseNamee;
 		addCourse(courseCodee, courseNamee);
@@ -61,7 +61,7 @@ void CourseCatalog::addCourses() {												//medlemsfunksjonen addCourses lar
 		
 
 	}
-	cout << " Course Catalog: " << endl;										//skriver ut en oversikt over emnene(emnekatalog) på formen "emnekode : emnenavn"
+	cout << " Course Catalog: " << endl;				//skriver ut en oversikt over emnene(emnekatalog) pÃ¥ formen "emnekode : emnenavn"
 	for (const auto& i : courseMap) {
 		cout << i.first << ": " << i.second << endl;
 	}
@@ -69,21 +69,21 @@ void CourseCatalog::addCourses() {												//medlemsfunksjonen addCourses lar
 
 void CourseCatalog::writeCourseCatalogToTxtFile() {
 	string nameTxtFile{ "" };
-	/*cout << "Du ønsker nok å lagre emnekatalogen din i en fil. Navngi filen du ønsker å lagre emnekatalogen i: " << endl; 
-	cin >> nameTxtFile;												//lar bruker navngi filen emnekatalogen skal lagres i 
+	/*cout << "Du Ã¸nsker nok Ã¥ lagre emnekatalogen din i en fil. Navngi filen du Ã¸nsker Ã¥ lagre emnekatalogen i: " << endl; 
+	cin >> nameTxtFile;						//lar bruker navngi filen emnekatalogen skal lagres i 
 	string fil_navn = nameTxtFile + ".txt";*/
 	string navn = "fil_navn.txt";
-	ofstream Offs{ navn };											//oppretter en output-stream, og kaller den Offs
+	ofstream Offs{ navn };						//oppretter en output-stream, og kaller den Offs
 	if (!Offs) {
 		error(" File not found. ");
 	}
-	for (const auto& j : courseMap) {							 //bruker auto til å itererer gjennom map
-		Offs << j.first + " : " + j.second + '\n';				//Ved bruk av Offstreamen min Offs
+	for (const auto& j : courseMap) {				//bruker auto til Ã¥ itererer gjennom map
+		Offs << j.first + " : " + j.second + '\n';		//Ved bruk av Offstreamen min Offs
 	}
 }
 
 void CourseCatalog::readFromTxtFile(string fil_navn) {
-	ifstream Iffs{ fil_navn }; //ifstream == cin for fil
+	ifstream Iffs{ fil_navn }; 			//ifstream kan tenkes pÃ¥ som "cin for fil"
 	string streng;
 	if (!Iffs) {
 		error("File not found. ");
@@ -94,14 +94,5 @@ void CourseCatalog::readFromTxtFile(string fil_navn) {
 	}
 }
 
-//getline bruker når du tar input strenger med mellomrom 
+//getline bruker nÃ¥r du tar input strenger med mellomrom (kan f.eks ikke skille med mellomrom med cin)
 //syntaks: getline(cin, string_variable, "evt. valgfritt linjeskifte, eks ":" ")
-
-//Bracket vs. insert 
-
-
-
-
-
-
-
