baccb402e119d5c492587aaf7b4d372a3e1731fc
Zeptu <42481829+Zeptu@users.noreply.github.com>
2019-02-22 10:36:44 +0100

    Update Oppgave_4.h

--- Oppgave_4.h
+++ Oppgave_4.h
@@ -1,8 +1,7 @@
 #pragma once
 #include "std_lib_facilities.h"
 
-//definer typen (enum class) temps:
-struct Temps {							//Husk: struct == klasse hvor alt er public ved default
+struct Temps {					//Husk: struct == klasse hvor alt er public ved default
 	double max;
 	double min;
 };
