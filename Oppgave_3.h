b305f2e8a5505c7e046faa405ae6b9cfec9d5610
Zeptu <42481829+Zeptu@users.noreply.github.com>
2019-02-22 10:46:52 +0100

    Update Oppgave_3.h

--- Oppgave_3.h
+++ Oppgave_3.h
@@ -4,22 +4,22 @@
 
 class CourseCatalog {
 private:
-	map<string, string> courseMap;											//Oppretter map
+	map<string, string> courseMap;							//Oppretter map
 
 public:
-	CourseCatalog() {}														//Oppretter konstruktør
-	friend ostream& operator << (ostream& os, const CourseCatalog& c);		//skriv ut alle emnekoder med tilhørende emnenavn
-	void addCourse(string courseName, string courseCode);					//legg til et kurs med emnekode og emnenavn
-	void removeCourse(string courseCode);									//fjern et kurs gitt av emnekoden
-	string getCourse(string courseCode);									//finn emnenavnet til et kurs med en gitt emnekode
-	void addCourses();														//lar bruker legge til emner i map
-	void readFromTxtFile(string fil_navn);									//leser fra fil
-	void writeCourseCatalogToTxtFile();										//skriver til fil
+	CourseCatalog() {}								//Oppretter konstruktÃ¸r
+	friend ostream& operator << (ostream& os, const CourseCatalog& c);		//skriv ut alle emnekoder med tilhÃ¸rende emnenavn
+	void addCourse(string courseName, string courseCode);				//legg til et kurs med emnekode og emnenavn
+	void removeCourse(string courseCode);						//fjern et kurs gitt av emnekoden
+	string getCourse(string courseCode);						//finn emnenavnet til et kurs med en gitt emnekode
+	void addCourses();								//lar bruker legge til emner i map
+	void readFromTxtFile(string fil_navn);						//leser fra fil
+	void writeCourseCatalogToTxtFile();						//skriver til fil
 									
 };
 
 /*
-Operatorer kan ikke være medlemsfunksjoner (for det meste), derfor har vi dem heller som friend.
-Ved å ha dem som friend får de tilgang på de PRIVATE delene av klassen, så i dette tilfellet gir
-vi operatoren tilgang på map. 
+Operatorer kan ikke vÃ¦re medlemsfunksjoner (for det meste), derfor har vi dem heller som friend.
+Ved Ã¥ ha dem som friend fÃ¥r de tilgang pÃ¥ de PRIVATE delene av klassen, sÃ¥ i dette tilfellet gir
+vi operatoren tilgang pÃ¥ map. 
 */
