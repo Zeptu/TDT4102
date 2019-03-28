ce20b4dbbf22c776a4e2cb635dfaa9063326217d
Zeptu <42481829+Zeptu@users.noreply.github.com>
2019-03-28 11:42:50 +0100

    Update Person.h

--- Oving 8/Person.h
+++ Oving 8/Person.h
@@ -6,25 +6,24 @@ class Person
 {
 private:
 	string name, email;
-	Car* car;											//privat medlemsvariabel car som er en peker til Car*
+	Car* car;		//privat medlemsvariabel car som er en peker til Car*
 
 public:
-	Person(string name, string email, Car* car = nullptr);	//Setter car sin defaul til nullptr, default-argumentt må være det siste i parameterlista
+	Person(string name, string email, Car* car = nullptr);	//Setter car sin defaul til nullptr, default-argumentt mÃ¥ vÃ¦re det siste i parameterlista
 	string getName() const;
 	string getEmail() const;
 	void setEmail( string nyemail );
 	bool hasAvailableSeats() const;
 
-	friend ostream& operator<<(ostream& os, const Person& person);   /*	Skal skrive ut innholdet i Person til en ostream
-																		må deklareres friend for å få tilgang på klassen, operatorer kan ikke være medlemsfunksjoner. Friend-deklarasjon
-																		gir operatoren tilgang på den delen av klassen som er deklarert private.
-																	 */
+	friend ostream& operator<<(ostream& os, const Person& person); /*Skal skrive ut innholdet i Person til en ostream
+									mÃ¥ deklareres friend for Ã¥ fÃ¥ tilgang pÃ¥ klassen, operatorer kan ikke vÃ¦re medlemsfunksjoner. Friend-deklarasjon
+									gir operatoren tilgang pÃ¥ den delen av klassen som er deklarert private.*/
 };
 /*
-Drøft:
-- Operatorer bør deklareres med const-parametere fordi ....
+DrÃ¸ft:
+- Operatorer bÃ¸r deklareres med const-parametere fordi ....
 
-- Vi bør bruke const-parametere når ........
+- Vi bÃ¸r bruke const-parametere nÃ¥r ........
 
-- Vi KAN IKKE bruke const-parametere når ........
+- Vi KAN IKKE bruke const-parametere nÃ¥r ........
 */
