617a2ef537d7af7c64c5fb7f1e8aa70eb6e21e2c
Zeptu <42481829+Zeptu@users.noreply.github.com>
2019-05-10 10:59:39 +0200

    Update Emojii.h

--- Oving 7/Emojii.h
+++ Oving 7/Emojii.h
@@ -8,37 +8,37 @@
 
 using namespace Graph_lib;							// ???
 
-class Emoji											//Emoji er en abstrakt klasse. Emoji er Baseklassen vår. 
-{													//Må implementere medlemsfunksjonen attach_to()
+class Emoji								//Emoji er en abstrakt klasse. Emoji er Baseklassen vÃ¥r. 
+{									//MÃ¥ implementere medlemsfunksjonen attach_to()
 public:
-	Emoji(const Emoji&) = delete;					//Disable copying. Disable slicing, etc. //Sletter kopikonstruktøren
+	Emoji(const Emoji&) = delete;					//Disable copying. Disable slicing, etc. //Sletter kopikonstruktÃ¸ren
 
-	Emoji& operator=(const Emoji&) = delete;		//Sletter tilordningsoperatoren i Emoji
+	Emoji& operator=(const Emoji&) = delete;			//Sletter tilordningsoperatoren i Emoji
 
-													// Deleting the copy constructor also deletes the default constructor.
-													// Emoji needs a default constructor.
-	Emoji() {}										//Oppretter default konstruktør for Emoji, alternativt: ( Emoji() = default;), disse to metodene er ekvivalente
+									// Deleting the copy constructor also deletes the default constructor.
+									// Emoji needs a default constructor.
+	Emoji() {}							//Oppretter default konstruktÃ¸r for Emoji, alternativt: ( Emoji() = default;), disse to metodene er ekvivalente
 	
 	virtual void attach_to(Graph_lib::Window&) = 0;	// The pure virtual function that has to be overriden for a deriving class
-													// to be instantiable. Every class deriving from Emoji is supposed to
-													// attach all its Shapes to a window. This makes the class abstract.
+							// to be instantiable. Every class deriving from Emoji is supposed to
+							// attach all its Shapes to a window. This makes the class abstract.
 
-													// Relevant because Vector_ref can own Emojis and automatically cleans up.
+							// Relevant because Vector_ref can own Emojis and automatically cleans up.
 
-	virtual ~Emoji() {}								//Virtuell destruktør
+	virtual ~Emoji() {}				//Virtuell destruktÃ¸r
 };
 
 /*
 VIKTIG:
 - Kun Virtual-funksjoner kan overskrives
-- PureVirtual-funksjoner MÅ overskrives
-	Betyr: Du får ikke bruke denne funksjoner på objekter direkte, men du kan bruke den i underklassene
+- PureVirtual-funksjoner MÃ… overskrives
+	->Betyr: Du fÃ¥r ikke bruke denne funksjoner pÃ¥ objekter direkte, men du kan bruke den i underklassene
 */
 
 
 
-class Face : public Emoji							//Face er en abstrakt klasse som inneholder et tomt, gult ansikt
-													//Face arver fra Emoji
+class Face : public Emoji				//Face er en abstrakt klasse som inneholder et tomt, gult ansikt
+							//Face arver fra Emoji
 {
 protected:			
 	Circle c1;
@@ -52,18 +52,18 @@ public:
 
 	virtual ~Face() {}
 
-	Face(Point c, int r);												//Deklarerer konstruktør med parametre
-	virtual void attach_to(Graph_lib::Window& win) override = 0;		//Deklarerer attach_to, overskriver, pure virtual
+	Face(Point c, int r);						//Deklarerer konstruktÃ¸r med parametre
+	virtual void attach_to(Graph_lib::Window& win) override = 0;	//Deklarerer attach_to, overskriver, pure virtual
 };
 
 
-class EmptyFace : public Face {											//Sub-klassen EmptyFace
+class EmptyFace : public Face {						//Sub-klassen EmptyFace
 protected: 
-	Circle e1, e2;														//Øynene
+	Circle e1, e2;							//Ã˜ynene
 
 public:
-	EmptyFace(Point center, int radius);								//Deklarerer konstruktør med parametre
-	void attach_to(Graph_lib::Window& win) override;					//Deklarerer attach_to
+	EmptyFace(Point center, int radius);				//Deklarerer konstruktÃ¸r med parametre
+	void attach_to(Graph_lib::Window& win) override;		//Deklarerer attach_to
 };
 
 class SmileyEmoji : public EmptyFace {
@@ -98,7 +98,7 @@ public:
 class AngryEmoji : public EmptyFace {
 protected:
 	Arc a4;
-	Line l1, l2;									//Øyenbryn
+	Line l1, l2;							//Ã˜yenbryn
 
 public:
 	AngryEmoji(Point center, int radius);
@@ -107,8 +107,8 @@ public:
 
 class EmojiWithGlasses : public EmptyFace {
 protected:
-	Arc a5, a6, a7;									//Smilefjes og briller
-	Line l3;										//Strek mellom brillene
+	Arc a5, a6, a7;							//Smilefjes og briller
+	Line l3;							//Strek mellom brillene
 public:
 	EmojiWithGlasses(Point center, int radius);
 	void attach_to(Graph_lib::Window& win) override;
